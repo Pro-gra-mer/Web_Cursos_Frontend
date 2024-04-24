fb8a31921e8d106691dcfa4012ebb87c8c1fccfc
Rebeca <rbk_po@hotmail.com>
2024-04-24 16:35:55 +0200

    Inicio frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Web Cursos</h1>
+      <div>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+import { ReactComponent as LogoWhite } from "./svg/tincode-white.svg";
+import authBg from "./jpg/auth-bg.jpg";
+import homeBanner from "./jpg/home-banner.jpg";
+import noAvatar from "./jpg/no-avatar.jpg";
+import academyLogo from "./png/academy-logo.png";
+
+const Icon = {
+  LogoWhite,
+};
+
+const image = {
+  authBg,
+  homeBanner,
+  noAvatar,
+  academyLogo,
+};
+
+export { Icon, image };
